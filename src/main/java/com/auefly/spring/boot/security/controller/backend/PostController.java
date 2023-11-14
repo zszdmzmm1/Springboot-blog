99a2651f614fb694c032439aa4e67f4472a7c16f
zszdmzmm <1075534380@qq.com>
2023-11-14 16:19:42 +0800

    post index

--- src/main/java/com/auefly/spring/boot/security/controller/backend/PostController.java
+++ src/main/java/com/auefly/spring/boot/security/controller/backend/PostController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Controller
+@Controller("backendPostController")
 public class PostController {
     @Autowired
     PostService postService;
