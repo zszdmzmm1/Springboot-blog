6d0b1baa70f880aae9376909f50cc2e2c7492255
zszdmzmm <1075534380@qq.com>
2023-11-15 16:24:11 +0800

    resource show

--- src/main/java/com/auefly/spring/boot/security/controller/ResourceController.java
+++ src/main/java/com/auefly/spring/boot/security/controller/ResourceController.java
@@ -4,10 +4,13 @@ import com.auefly.spring.boot.security.entity.Post;
 import com.auefly.spring.boot.security.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -25,5 +28,17 @@ public class ResourceController {
         Page<Post> optionalPost = postService.findAllResources(currentPage, pageSize);
         model.addAttribute("page", optionalPost);
         return "resource/index";
+    }
+
+    @GetMapping("/resources/{id}")
+    String show(@PathVariable Long id, Model model) {
+        Optional<Post> optionalPost = postService.findById(id);
+
+        if(optionalPost.isEmpty() || !optionalPost.get().isStatus()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        model.addAttribute("post", optionalPost.get());
+        return "resource/show";
     }
 }
